0841c9fc2ec3911fe6215611677193cfe81b7eb0
leogottardo1 <leonardo.28@edu.unipar.br>
2024-05-10 08:15:13 -0300

    arquivo script

---
+++ galeria de imagens/scripts-basico.js
@@ -0,0 +1,22 @@
+//o vento DOM é acionado quando todo o HTML foi completamente carregado
+document.addEventListener("DOMContentLoaded", function () {
+    alert(123);
+  
+    //tipos de dados
+  //String: Sequencia de caracter
+  //Number: numeros inteiros ou pontos flutuantes (10.4)
+  //boolean; Retorna dois valores, true ou false
+  //object: estrutura de dados qque pode armazenar multiplos valores
+  //null: quando esta vazio
+  //array: objeto especial ordenado
+  //fuction: bloco de código reutilizavel
+  
+  //é usado para declarar uma variavel cuja valor é permanente 
+  const test = 50
+  
+  // LET é usado para declarar variaveis que nao podem ser 
+  // retribuidos
+  let C= 0;
+  C = 10
+  console.log(C)
+});
