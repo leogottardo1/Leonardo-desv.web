6cbae18c8dc2410281151b6bf052b3c2d2a2ee13
leogottardo1 <leonardo.28@edu.unipar.br>
2024-05-29 20:22:51 -0300

    arquivo

--- galeria-imagens/scripts.js
+++ galeria-imagens/scripts.js
@@ -1,34 +1,50 @@
 document.addEventListener('DOMContentLoaded',function(){ 
     
-    const baseimagens = {
+    const baseImagens = {
         todas: ['imagem.png', 'cidade.jpg', 'animais.jpg', 'natureza.jpg'],
         natureza: ['natureza.jpg'],
         cidade: ['cidade.jpg'],
         animais: ['animais.jpg'],
     }
 
-function carregaImagens(){
+function carregaImagens(categoria){
     //selecionar elementos HTML com base em um seletor
     const galeria = document.querySelector("#galeria-imagens");
-
-    const imagens = baseimagens['todas'];
+    galeria.innerHTML='';//limpar bloco, apaga tudo dentro
+    const imagens = baseImagens[categoria];
 
     imagens.forEach(img => { 
-    console.log(img);
-        galeria.innerHTML += '<div> <img src="imagens/'+img+'"/>  </div>'
+    
+        galeria.innerHTML += '<div class"imagem-item"> <img src="imagens/'+img+'"/>  </div>'
+    })
+    
+}
+
+function ordenaImagens(ordem){
+    
+    const imagens = Array.from(document.querySelectorAll("#galeria-imagens .imagem-item"));
+
+    imagens.sort((a, b)=>{
+        console.log(a);
+
     })
 
 }
-
-carregaImagens();
 
     //evento de click
     document.body.addEventListener('click', function(event) {
         //acessa a lista de classe de onde clicou e 
         //procura pela classe botao-categoria
         if (event.target.classList.contains('botao-categoria')) { 
-            alert(321);
-        }
-    })
+          
+            const categoria = event.target.dataset.categoria;
+            carregaImagens(categoria);
+    }
+    if(event.target.classList.contains('botao-ordenar')){
+        const ordem = event.target.dataset.ordem;
+        ordenaImagens(ordem)  
+    }
+})
+carregaImagens('todas');
 
-})//eu
+})
